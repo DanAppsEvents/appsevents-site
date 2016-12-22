b1ee3d08f878d27f9616de48b33079150b3110d0
deanappsevents <deanappsevents.com>
2016-12-22 15:19:09 +0000

    removed mailchimp js

--- assets/js/material-kit.js
+++ assets/js/material-kit.js
@@ -200,17 +200,3 @@ function isElementInViewport(elem) {
 
     return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
 }
-
-$(function() {
-  var bool = true, flag = false;
-  $('#mc-embedded-subscribe').prop('disabled', bool); // use prop to disable the button
-
-  $(document).keyup(function() { // listen the keyup on the document or you can change to form in case if you have or you can try the closest div which contains the text inputs
-    $('#mce-EMAIL').each(function() { // loop through each text inputs
-      bool = $.trim(this.value) === "" ?  true :  false; // update the var bool with boolean values
-      if(bool)
-      return flag;
-    });
-    $('#mc-embedded-subscribe').prop('disabled', bool); // and apply the boolean here to enable
-  });
-});
