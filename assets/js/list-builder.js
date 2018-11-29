df84818679501f5569e5033e95852f28969de852
Jane Vestil <jane@eventsframe.com>
2018-11-29 19:48:14 +0800

    newsletter pop-up

---
+++ assets/js/list-builder.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+    var delay = 10000; // milliseconds
+    var cookie_expire = 0; // days
+
+    var cookie = localStorage.getItem("list-builder");
+    if(cookie == undefined || cookie == null) {
+        cookie = 0;
+    }
+
+    if(((new Date()).getTime() - cookie) / (1000 * 60 * 60 * 24) > cookie_expire) {
+        $("#list-builder").delay(delay).fadeIn("fast", () => {
+            $("#popup-box").fadeIn("fast", () => {});
+        });
+
+        $("button[name=subscribe]").click(() => {
+            $.ajax({
+                type: "POST",
+                url: $("#popup-form").attr("action"),
+                data: $("#popup-form").serialize(),
+                success: (data) => {
+                    $("#popup-box-content").html("<p style='text-align: center'>Thank you for subscribing to The AppsEvents newsletter!</p>");
+                }
+            });
+        });
+
+        $("#popup-close").click(() => {
+            $("#list-builder, #popup-box").hide();
+            localStorage.setItem("list-builder", (new Date()).getTime());
+        });
+    }
+
+});
